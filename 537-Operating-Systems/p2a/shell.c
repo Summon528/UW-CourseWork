d466e202162ba1c5e690bdba854bc978cee5a4bf
Chang-Yen Tseng <tsengcy@cs.wisc.edu>
2021-10-01 16:53:48 -0500

    OS: P2a implement most features

--- 537-Operating-Systems/p2a/shell.c
+++ 537-Operating-Systems/p2a/shell.c
@@ -1,4 +1,3 @@
-
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -9,26 +8,21 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+char error_message[30] = "An error has occurred\n";
 typedef struct Input {
   bool redirect;
   char** cmd;
   char* filename;
+  char*** looploc;
 } Input_t;
 
-Input_t* get_input() {
-  char *line = NULL, *token = NULL;
-  size_t sz = 0;
+Input_t* parse_input(char* line) {
   Input_t* input = NULL;
-  int cmdlen = 1;
-  printf("wish> ");
-  if (getline(&line, &sz, stdin) == EOF) {
-    exit(0);
-  }
-  line[strlen(line) - 1] = '\0';
+  char* token = NULL;
+  char** duptoken = NULL;
+  int cmdcnt = 1, loopcnt = 0;
   token = strtok(line, " ");
-  if (token == NULL) {
-    goto get_input_end;
-  }
+  if (token == NULL) return NULL;
   input = malloc(sizeof(Input_t));
   memset(input, 0, sizeof(Input_t));
   input->cmd = malloc(sizeof(char*));
@@ -39,27 +33,25 @@ Input_t* get_input() {
       continue;
     }
     if (!input->redirect) {
-      input->cmd = realloc(input->cmd, sizeof(char*) * (cmdlen + 1));
-      cmdlen++;
-      input->cmd[cmdlen - 1] = strdup(token);
+      input->cmd = realloc(input->cmd, sizeof(char*) * (cmdcnt + 1));
+      input->cmd[cmdcnt] = strdup(token);
+      duptoken = &input->cmd[cmdcnt];
+      cmdcnt++;
     } else {
       input->filename = strdup(token);
+      duptoken = &input->filename;
+    }
+    if (strcmp(token, "$loop") == 0) {
+      input->looploc = realloc(input->looploc, sizeof(char**) * (loopcnt + 1));
+      input->looploc[loopcnt] = duptoken;
+      loopcnt++;
     }
   }
-  input->cmd = realloc(input->cmd, sizeof(char*) * (cmdlen + 1));
-  input->cmd[cmdlen] = NULL;
-
-get_input_end:
-  free(line);
+  input->cmd = realloc(input->cmd, sizeof(char*) * (cmdcnt + 1));
+  input->cmd[cmdcnt] = NULL;
+  input->looploc = realloc(input->looploc, sizeof(char*) * (loopcnt + 1));
+  input->looploc[loopcnt] = NULL;
   return input;
-}
-
-void replace_proc(char** cmd) {
-  int r = execvp(cmd[0], cmd);
-  if (r == -1) {
-    fprintf(stderr, "failed: %s", cmd[0]);
-    exit(EXIT_FAILURE);
-  }
 }
 
 void exec(const Input_t* input) {
@@ -72,20 +64,69 @@ void exec(const Input_t* input) {
       dup2(fd, STDOUT_FILENO);
       close(fd);
     }
-    replace_proc(input->cmd);
+    int r = execvp(input->cmd[0], input->cmd);
+    if (r == -1) {
+      fputs(error_message, stderr);
+      exit(EXIT_FAILURE);
+    }
   } else {
     waitpid(pid, NULL, 0);
   }
 }
 
-int main() {
-  Input_t* input = NULL;
-  while (1) {
-    input = get_input();
-    if (input != NULL) {
+void run(Input_t* input) {
+  if (strcmp(input->cmd[0], "cd") == 0) {
+    chdir(input->cmd[1]);
+  } else if (strcmp(input->cmd[0], "exit") == 0) {
+    exit(0);
+  } else if (strcmp(input->cmd[0], "path") == 0) {
+    char* path = NULL;
+    int pathlen = 0;
+    for (int i = 1; input->cmd[i]; i++) {
+      if (pathlen) path[pathlen - 1] = ':';
+      pathlen += strlen(input->cmd[i]) + 1;
+      path = realloc(path, sizeof(char) * (pathlen + 1));
+      strcat(path, input->cmd[i]);
+      path[pathlen] = '\0';
+    }
+    setenv("PATH", path, 1);
+  } else if (strcmp(input->cmd[0], "loop") == 0) {
+    int cnt = atoi(input->cmd[1]);
+    free(input->cmd[0]);
+    free(input->cmd[1]);
+    input->cmd += 2;
+    for (int i = 0; i < cnt; i++) {
+      char numstr[20];
+      sprintf(numstr, "%d", i + 1);
+      for (int j = 0; input->looploc[j]; j++) {
+        free(*input->looploc[j]);
+        *input->looploc[j] = strdup(numstr);
+      }
       exec(input);
+    }
+  } else {
+    exec(input);
+  }
+}
+
+int main() {
+  setenv("PATH", "/bin", true);
+  while (true) {
+    Input_t* input = NULL;
+    char* line = NULL;
+    size_t sz = 0;
+    printf("wish> ");
+    if (getline(&line, &sz, stdin) == EOF) break;
+    line[strlen(line) - 1] = '\0';
+    input = parse_input(line);
+    if (input != NULL) {
+      run(input);
+      for (int i = 0; input->cmd[i]; i++) {
+        free(input->cmd[i]);
+      }
       free(input);
     }
+    free(line);
   }
   return 0;
 }
