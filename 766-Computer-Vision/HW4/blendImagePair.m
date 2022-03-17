79d46bc88d26b63583c41b8b11f82a157b11bb06
Chang-Yen Tseng <10328757+Summon528@users.noreply.github.com>
2022-03-17 11:46:37 -0500

    CV: HW4 1c 1d

--- 766-Computer-Vision/HW4/blendImagePair.m
+++ 766-Computer-Vision/HW4/blendImagePair.m
@@ -1,1 +1,35 @@
 function out_img = blendImagePair(wrapped_imgs, masks, wrapped_imgd, maskd, mode)
+    og_class = class(wrapped_imgs);
+    wrapped_imgs = cast(wrapped_imgs, "double");
+    masks = cast(masks, "double");
+    wrapped_imgd = cast(wrapped_imgd, "double");
+    maskd = cast(maskd, "double");
+    
+    [rows, cols] = size(masks);
+    out_img = zeros(rows, cols, 3, class(wrapped_imgs));
+    if mode == "overlay"
+        maskd2 = maskd > 0;
+        masks2 = masks - maskd;
+        masks2 = masks2 > 0;
+    else
+        masks2 = bwdist(masks == 0, 'euclidean');
+        masks2 = masks2 / max(masks2, [], 'all');
+        maskd2 = bwdist(maskd == 0, 'euclidean');
+        maskd2 = maskd2 / max(maskd2, [], 'all');
+        mask_sum = masks2 + maskd2;
+        mask_sum = mask_sum / max(mask_sum, [], 'all');
+        mask_sum(mask_sum == 0) = 1;
+        masks2 = masks2 ./ mask_sum;
+        masks2 = masks2 / max(masks2, [], 'all');
+        maskd2 = maskd2 ./ mask_sum;
+        maskd2 = maskd2 / max(maskd2, [], 'all');
+    end
+    for i=1:rows
+        for j=1:cols
+               out_img(i, j, :) = wrapped_imgd(i, j, :) * maskd2(i, j) ...
+                    + wrapped_imgs(i, j, :) * masks2(i, j);
+        end
+    end
+    
+    out_img = cast(out_img, og_class);
+             
--- 766-Computer-Vision/HW4/runRANSAC.m
+++ 766-Computer-Vision/HW4/runRANSAC.m
@@ -1,1 +1,18 @@
 function [inliers_id, H] = runRANSAC(Xs, Xd, ransac_n, eps)
+    inliers_id = [];
+    H = [];
+    cnt = size(Xs, 1);
+    for i=1:ransac_n
+        idxs = randperm(cnt, 4);
+        H_3x3 = computeHomography(Xs(idxs, :), Xd(idxs, :));
+        xd_calc = applyHomography(H_3x3, Xs);
+        dist = (xd_calc(:, 1) - Xd(:, 1)).^2 + (xd_calc(:, 2) - Xd(:, 2)).^2;
+        tmp = find(dist < eps);
+        if size(tmp, 1) > size(inliers_id, 1)
+            inliers_id = tmp;
+            H = H_3x3;
+        end
+    end
+    
+
+
