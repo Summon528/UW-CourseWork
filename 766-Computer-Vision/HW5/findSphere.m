315f37eb0806de72a42cf1c1f18f762bb6268e76
Chang-Yen Tseng <10328757+Summon528@users.noreply.github.com>
2022-03-25 11:04:37 -0500

    CV: HW5 1a

--- 766-Computer-Vision/HW5/findSphere.m
+++ 766-Computer-Vision/HW5/findSphere.m
@@ -1,1 +1,17 @@
 function [center, radius] = findSphere(img)
+    [rows, cols] = size(img);
+    bwimg = im2bw(img, 0);
+    x_sum = 0;
+    y_sum = 0;
+    area = 0;
+    for x=1:cols
+        for y=1:rows
+            if bwimg(y, x)
+                x_sum = x_sum + x;
+                y_sum = y_sum + y;
+                area = area + 1;
+            end
+        end
+    end
+    center = [x_sum / area, y_sum / area];
+    radius =  sqrt(area/pi);
