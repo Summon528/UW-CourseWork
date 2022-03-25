bea1035ab085f9f29091f94abc1646cd35988f9c
Chang-Yen Tseng <10328757+Summon528@users.noreply.github.com>
2022-03-25 12:25:12 -0500

    CV: HW5 1b 1c

--- 766-Computer-Vision/HW5/computeLightDirections.m
+++ 766-Computer-Vision/HW5/computeLightDirections.m
@@ -1,1 +1,13 @@
 function light_dirs_5x3 = computeLightDirections(center, radius, img_cell)
+    light_dirs_5x3 = zeros(5, 3);
+
+    for i=1:size(img_cell, 1)
+        img = img_cell{i};
+        mx = max(img,[],'all');
+        [Y, X] = ind2sub(size(img), find(img == mx));
+        xx = mean(X) - center(1);
+        yy = mean(Y) - center(2);
+        zz = sqrt(radius^2 - xx^2 - yy^2);
+        dir = [xx yy zz] / norm([xx yy zz]) .* double(mx);
+        light_dirs_5x3(i, :) = dir;
+    end
--- 766-Computer-Vision/HW5/computeMask.m
+++ 766-Computer-Vision/HW5/computeMask.m
@@ -1,1 +1,5 @@
 function mask = computeMask(img_cell)
+    mask = img_cell{1} ~= 0;
+    for i=2:size(img_cell, 1)
+        mask = bitor(mask, img_cell{i} ~= 0);
+    end
